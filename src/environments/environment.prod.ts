c4c70940adb27b571a6c41cbfeb7011ca9a19f97
Tilo Germer <tilo.germer@t-online.de>
2018-05-02 17:01:20 +0200

    Angular4 crud with firestore database

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBwiaf1NfiJC88Ch5_-WiNnJoM2rclMJGk',
+    authDomain: 'fs1prod-e0f83.firebaseapp.com',
+    databaseURL: 'https://fs1prod-e0f83.firebaseio.com',
+    projectId: 'fs1prod-e0f83',
+    storageBucket: 'fs1prod-e0f83.appspot.com',
+    messagingSenderId: '48392927223'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBwiaf1NfiJC88Ch5_-WiNnJoM2rclMJGk',
+    authDomain: 'fs1prod-e0f83.firebaseapp.com',
+    databaseURL: 'https://fs1prod-e0f83.firebaseio.com',
+    projectId: 'fs1prod-e0f83',
+    storageBucket: 'fs1prod-e0f83.appspot.com',
+    messagingSenderId: '48392927223'
+  }
+};
